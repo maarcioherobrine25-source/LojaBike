b5159d30a7e63496d0d455a8662fdbf97b870f98
Guilherme <maarcioherobrine25@gmail.com>
2026-08-17 20:06:30 -0300

    feat: Criando as funçoes da classe

--- LojaBike/Bike.cs
+++ LojaBike/Bike.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LojaBike
+﻿namespace LojaBike
 {
-    internal class Bike
+    class Bike
     {
+        public string Modelo { get; set; } = string.Empty;
+        public double Bateria { get; set; }
+        public double PesoCiclista { get; set; }
+        public int RecargaMes { get; set; }
+
+        public double CalcularAutonomia()
+        {
+            double autonomiaBase = Bateria / 15.0;
+            bool pesoElevado = PesoCiclista > 90;
+
+            return pesoElevado ? (autonomiaBase * 0.05) : autonomiaBase;
+        }
+
+        public double CalcularConsumoMensal()
+        {
+            double kmPorCarga = Bateria / 1000.0;
+            return kmPorCarga * RecargaMes;
+        }
+
+        public double CalcularCustoMensal()
+        {
+            const double PRECO_KWH = 0.80;
+            return CalcularConsumoMensal() * PRECO_KWH;
+        }
+
+        public bool ModeloEconomico()
+        {
+            return (CalcularCustoMensal() < 15.00) || (CalcularAutonomia() > 40.0);
+        }
     }
 }
